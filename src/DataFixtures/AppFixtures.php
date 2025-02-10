c2d4f821da4976973b786e5228e8cf05b45f7278
Noobos100 <charles.daw@etu.univ-amu.fr>
2025-02-10 10:58:08 +0100

    add: answers and questions entities and fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,7 +2,8 @@
 
 namespace App\DataFixtures;
 
-use App\Entity\User;
+use App\Entity\Question;
+use App\Entity\Answer;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -10,11 +11,61 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        for ($i = 0; $i < 300; $i++) {
-            $user = new User();
-            $user->setEmail('a.a' . $i . 'gmail.com');
-            $manager->persist($user);
-        }
+        # Personality test questions and their answers
+        $question1 = new Question();
+        $question1->setText('Les gens s\'ennuiraient sans moi.');
+        $this->AssignAnswer($manager, $question1, 'Non', 'Mouais', 'Grave');
+
+        $question2 = new Question();
+        $question2->setText('Une soirée idéale pour toi, c\'est...');
+        $this->AssignAnswer($manager, $question2, 'Netflix & chill', 'Un bon resto', 'Une soirée jeux de société');
+
+        $question3 = new Question();
+        $question3->setText('Tu es plutôt...');
+        $this->AssignAnswer($manager, $question3, 'Du matin', 'Du soir', 'De l\'après-midi');
+
+        $question4 = new Question();
+        $question4->setText('Tes derniers mots avant de partir...');
+        $this->AssignAnswer($manager, $question4, 'Je vous aime', 'Je reviendrai', 'Je reviens');
+
+        $manager->persist($question1);
+        $manager->persist($question2);
+        $manager->persist($question3);
+        $manager->persist($question4);
+
+        $manager->flush();
+    }
+
+    /**
+     * @param ObjectManager $manager
+     * @param Question $question
+     * @param $answerText1
+     * @param $answerText2
+     * @param $answerText3
+     * @return void
+     */
+    public function AssignAnswer(ObjectManager $manager, Question $question, $answerText1, $answerText2, $answerText3): void
+    {
+        $manager->persist($question);
+
+        $answer1 = new Answer();
+        $answer1->setText($answerText1);
+        $answer1->setScore(1);
+        $answer1->setQuestion($question);
+        $manager->persist($answer1);
+
+        $answer2 = new Answer();
+        $answer2->setText($answerText2);
+        $answer2->setScore(2);
+        $answer2->setQuestion($question);
+        $manager->persist($answer2);
+
+        $answer3 = new Answer();
+        $answer3->setText($answerText3);
+        $answer3->setScore(3);
+        $answer3->setQuestion($question);
+        $manager->persist($answer3);
+
         $manager->flush();
     }
 }
---
+++ src/Entity/Answer.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Metadata\ApiResource;
+use App\Repository\AnswerRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: AnswerRepository::class)]
+#[ApiResource]
+class Answer
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $text = null;
+
+    #[ORM\Column]
+    private ?int $score = null;
+
+    #[ORM\ManyToOne(inversedBy: 'answers')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?Question $question = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getText(): ?string
+    {
+        return $this->text;
+    }
+
+    public function setText(string $text): static
+    {
+        $this->text = $text;
+
+        return $this;
+    }
+
+    public function getScore(): ?int
+    {
+        return $this->score;
+    }
+
+    public function setScore(int $score): static
+    {
+        $this->score = $score;
+
+        return $this;
+    }
+
+    public function getQuestion(): ?Question
+    {
+        return $this->question;
+    }
+
+    public function setQuestion(?Question $question): static
+    {
+        $this->question = $question;
+
+        return $this;
+    }
+}
